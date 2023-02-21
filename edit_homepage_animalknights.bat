233a7f80960317a7529a5cd5360f2ad1ae22ef05
Schneehase007 <karina24h.glock@gmail.com>
2023-02-21 18:27:22 +0100

    edit gitignore

---
+++ edit_homepage_animalknights.bat
@@ -0,0 +1,26 @@
+@echo off
+rem Bypass "Terminate Batch Job" prompt.
+if "%~1"=="-FIXED_CTRL_C" (
+   REM Remove the -FIXED_CTRL_C parameter
+   SHIFT
+) ELSE (
+   REM Run the batch with <NUL and -FIXED_CTRL_C
+   CALL <NUL %0 -FIXED_CTRL_C %*
+   GOTO :GIT
+)
+echo animalhnights.at workflow...
+echo 
+echo pull from GitHub
+cd Homepage
+git pull origin master
+
+.\hugo server
+
+:GIT
+git add *
+echo 
+set /p response=please commit changes:
+echo %response%
+git commit -m "%response%"
+git push origin master
+ 
